779a61388a61d89b866fde277c5186f92a22feb1
freakbeast14 <shahilmohammed14@gmail.com>
2021-07-12 19:41:00 +0530

    components part 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
             <NavbarBrand href='/'>Navbar</NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes = {DISHES}/>
     </div>
   );
 }
